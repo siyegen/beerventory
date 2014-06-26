214e56ff4da7c6621cfd14e53dd6e46ddb3ef360
David Tomberlin <siyegen@gmail.com>
2014-06-26 14:48:11 -0700

    added api routes that do nothing

--- src/beerventory/main.go
+++ src/beerventory/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-martini/martini"
 )
 
 func main() {
 	fmt.Println("Drink beer")
+	m := martini.Classic()
+
+	m.Get("/beer", func() string {
+		return "beer"
+	})
+
+	m.Post("/beer", func() string {
+		return "added beer"
+	})
+
+	m.Put("/beer/:id", func(params martini.Params) string {
+		return fmt.Sprintf("edited beer %d", params["id"])
+	})
+
+	m.Delete("/beer/:id", func(params martini.Params) string {
+		return fmt.Sprintf("deleted beer %d", params["id"])
+	})
+
+	m.Post("/checkout", func() string {
+		return "added beer"
+	})
+
+	m.Run()
 }
